1539eff62828cd69914b3b3a7a21e96beec52a65
comuniam <celsocomu@gmail.com>
2025-07-05 22:33:19 -0300

    Update grupo1Tema3.py

--- grupo1Tema3.py
+++ grupo1Tema3.py
@@ -1,9 +1,7 @@
 #Celso Comuniam Pereira
 #Ruylis Bialta
 
-
 class Diario_de_bordo: #Criação da classe Diario_de_bordo
-
     def __init__(self, viagens, datas, locais): #Criado a definições para viagens, data e locais  
         try: #Faz a tentativa de inserção dos comandos abaixo
             self.viagens = viagens  
@@ -22,8 +20,6 @@ class Diario_de_bordo: #Criação da classe Diario_de_bordo
     def resumo_viagem(self):  # Método sobrescrito - polimorfismo
         return f"Resumo: Viagem para {self.locais} na data {self.datas}."
 
-
-
 class ViagemInternacional(Diario_de_bordo): #Classe filha que herda de Diario_de_bordo
     def __init__(self, viagens, datas, locais, idioma): #Novo atributo criado: idioma
         super().__init__(viagens, datas, locais) #Chama o construtor da classe mãe
@@ -32,14 +28,13 @@ class ViagemInternacional(Diario_de_bordo): #Classe filha que herda de Diario_de
     def resumo_viagem(self): #Sobrescrevendo o método da classe mãe - polimorfismo
         return f"[Internacional] Viagem para {self.locais}, em {self.datas}, idioma local: {self.idioma}."
 
-
 #Atribuição das informações para exibição 
-print("\n--- Criando viagem nacional ---")
+print("\nCriando viagem nacional")
 viagem_nacional = Diario_de_bordo(1, "10/02/2025", "João Pessoa")
 print(viagem_nacional.cadastro("Boa", 101))
 print(viagem_nacional.resumo_viagem())
 
-print("\n--- Criando viagem internacional ---")
+print("\nCriando viagem internacional")
 viagem_internacional = ViagemInternacional(2, "23/01/2025", "Lisboa", "Portugal")
-print(viagem_internacional.cadastro("Muito Boa!", 202))
+print(viagem_internacional.cadastro("Muito Boa", 202))
 print(viagem_internacional.resumo_viagem())
